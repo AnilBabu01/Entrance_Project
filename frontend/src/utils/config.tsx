1f969648ee039d46d1472cdd259870e2353f032c
AnilBabu01 <anilbabu3245@gmail.com>
2026-02-21 10:08:26 +0530

    working on frontend

--- frontend/src/utils/config.tsx
+++ frontend/src/utils/config.tsx
@@ -1,5 +1,5 @@
-export const BASE_URL ="https://devapi.studyitnepal.com/api/user"
-export const ORIGINAL_BASE_URL ="https://devapi.studyitnepal.com/"
+export const BASE_URL ="https://entrance.kkffgold.co.in/api/user"
+export const ORIGINAL_BASE_URL ="https://entrance.kkffgold.co.in/"
 
 // export const BASE_URL ="http://localhost:8000/api/user"
 // export const ORIGINAL_BASE_URL ="http://localhost:8000/"
